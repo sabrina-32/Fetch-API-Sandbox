9f4172205dcb906a146674cba62b6a611892c553
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-01-28 16:03:19 +0600

    4th commit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 document.getElementById('getText').addEventListener('click', getText);
 document.getElementById('getUsers').addEventListener('click', getUsers);
 document.getElementById('getPost').addEventListener('click', getPost);
+document.getElementById('addPost').addEventListener('submit', addPost);
 
 function getText()  {
 
@@ -59,4 +60,26 @@ function getPost() {
 
         document.getElementById('output').innerHTML = output;
     })
+}
+
+
+
+function  addPost(e){
+    e.preventDefault();
+
+    let  title = document.getElementById('title').value;
+    let  body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({title:title, body:body})
+    })
+
+    .then((res)=>res.json())
+    .then((data)=>console.log(data) )
+
 }
